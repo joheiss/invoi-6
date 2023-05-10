e0b11204f589b6bfd1f663d43ea18aec134d1993
Josef Heiss <josef.heiss@jovisco.de>
2023-05-10 14:31:30 +0200

    Phone numbers changed on invoice PDF, up to date firebase versions

--- functions/src/storage-functions.ts
+++ functions/src/storage-functions.ts
@@ -1,78 +1,97 @@
-import * as admin from 'firebase-admin';
-import {ObjectMetadata} from 'firebase-functions/lib/providers/storage';
-import * as path from 'path';
-import * as os from 'os';
-import * as _ from 'lodash';
-import * as sharp from 'sharp';
-import {EventContext} from 'firebase-functions';
+import * as admin from "firebase-admin";
+// import {ObjectMetadata} from 'firebase-functions/lib/providers/storage';
+import * as path from "path";
+import * as os from "os";
+import * as _ from "lodash";
+import * as sharp from "sharp";
+import { EventContext } from "firebase-functions";
+import { ObjectMetadata } from "firebase-functions/v1/storage";
 
-export async function handleDocumentDeletion(object: ObjectMetadata, context: EventContext): Promise<any> {
+export async function handleDocumentDeletion(
+  object: ObjectMetadata,
+  context: EventContext
+): Promise<any> {
   const filePath = object.name;
   const metadata = object.metadata;
-  if (filePath.startsWith('images/users/')) {
+  if (filePath.startsWith("images/users/")) {
     return false;
   }
-  if (!metadata || !metadata['owner']) {
+  if (!metadata || !metadata["owner"]) {
     return false;
   }
   return deleteDocumentLink(object.name);
 }
 
-export async function handleDocumentFinalization(object: ObjectMetadata, context: EventContext): Promise<any> {
+export async function handleDocumentFinalization(
+  object: ObjectMetadata,
+  context: EventContext
+): Promise<any> {
   const filePath = object.name;
   const metadata = object.metadata;
-  if (filePath.startsWith('images/users/')) {
+  if (filePath.startsWith("images/users/")) {
     return handleUserThumbnail(object, context);
   }
-  if (!metadata || !metadata['owner']) {
+  if (!metadata || !metadata["owner"]) {
     return false;
   }
   // create or update
   const documentLink = {
     name: path.basename(object.name),
     path: object.name,
-    type: +metadata['type'],
-    attachToEmail: (metadata['attachToEmail']) === 'true',
-    owner: metadata['owner']
+    type: +metadata["type"],
+    attachToEmail: metadata["attachToEmail"] === "true",
+    owner: metadata["owner"],
   };
   return updateDocumentLink(documentLink);
 }
 
-export async function handleMetadataUpdate(object: ObjectMetadata, context: EventContext): Promise<any> {
+export async function handleMetadataUpdate(
+  object: ObjectMetadata,
+  context: EventContext
+): Promise<any> {
   const filePath = object.name;
   const metadata = object.metadata;
   return false;
 }
 
-export async function handleUserThumbnail(object: ObjectMetadata, context: EventContext): Promise<any> {
+export async function handleUserThumbnail(
+  object: ObjectMetadata,
+  context: EventContext
+): Promise<any> {
   const fileBucket = object.bucket;
   const filePath = object.name;
   const contentType = object.contentType;
   const metadata = object.metadata;
   const SIZES = [32, 64]; // [64, 256, 512];
-  if (!contentType.startsWith('image/')) {
+  if (!contentType.startsWith("image/")) {
     console.log(`File at ${filePath} is not an image. No further processing.`);
     return false;
   }
-  if (_.includes(filePath, '_thumb')) {
+  if (_.includes(filePath, "_thumb")) {
     console.log(`File at ${filePath} is already a thumbnail.`);
     return false;
   }
-  const fileName = filePath.split('/').pop();
+  const fileName = filePath.split("/").pop();
   const tempFilePath = path.join(os.tmpdir(), fileName);
   const bucket = admin.storage().bucket(fileBucket);
-  return bucket.file(filePath).download({destination: tempFilePath})
+  return bucket
+    .file(filePath)
+    .download({ destination: tempFilePath })
     .then(() => {
       _.each(SIZES, (size, index) => {
         const newFileName = `profile-image_${size}_thumb.png`;
         const newFileTemp = path.join(os.tmpdir(), newFileName);
-        const newFilePath = `${filePath.substring(0, filePath.lastIndexOf('/'))}/thumbs/${newFileName}`;
+        const newFilePath = `${filePath.substring(
+          0,
+          filePath.lastIndexOf("/")
+        )}/thumbs/${newFileName}`;
         sharp(tempFilePath)
           .resize(size, null)
           .toFile(newFileTemp, (err, info) => {
-            bucket.upload(newFileTemp, {destination: newFilePath})
-              .then(file => file[0].makePublic())
-              .then(response => {
+            bucket
+              .upload(newFileTemp, { destination: newFilePath })
+              .then((file) => file[0].makePublic())
+              .then((response) => {
                 if (index === 0) {
                   updateUserProfile(newFilePath);
                 }
@@ -84,16 +103,20 @@ export async function handleUserThumbnail(object: ObjectMetadata, context: Event
 }
 
 export async function createDocumentLink(documentLink): Promise<any> {
-  return admin.firestore().collection('document-links').add(documentLink);
+  return admin.firestore().collection("document-links").add(documentLink);
 }
 
 export async function deleteDocumentLink(documentPath: string): Promise<any> {
-  const queryRef = await admin.firestore().collection('document-links').where('path', '==', documentPath).get();
+  const queryRef = await admin
+    .firestore()
+    .collection("document-links")
+    .where("path", "==", documentPath)
+    .get();
   if (queryRef.docs.length === 0) {
     return false;
   }
   const batch = admin.firestore().batch();
-  queryRef.docs.forEach(doc => batch.delete(doc.ref));
+  queryRef.docs.forEach((doc) => batch.delete(doc.ref));
   return batch.commit();
 }
 
@@ -103,19 +126,27 @@ export async function updateDocumentLink(documentLink): Promise<any> {
     return false;
   }
   const batch = admin.firestore().batch();
-  queryRef.docs.forEach(doc => batch.set(doc.ref, documentLink));
+  queryRef.docs.forEach((doc) => batch.set(doc.ref, documentLink));
   return batch.commit();
 }
 
 function getDocumentLinks(documentPath: string): Promise<any> {
-  return admin.firestore().collection('document-links').where('path', '==', documentPath).get();
+  return admin
+    .firestore()
+    .collection("document-links")
+    .where("path", "==", documentPath)
+    .get();
 }
 
 function updateUserProfile(filePath: string) {
   const projectId = process.env.GCLOUD_PROJECT;
-  console.log('Project Id: ', projectId);
+  console.log("Project Id: ", projectId);
   // const projectId = 'jovisco-invoicing';
   const downloadUrl = `https://storage.googleapis.com/${projectId}.appspot.com/${filePath}`;
-  const uid = filePath.split('/')[2];
-  return admin.firestore().collection('user-profiles').doc(uid).update({imageUrl: downloadUrl});
+  const uid = filePath.split("/")[2];
+  return admin
+    .firestore()
+    .collection("user-profiles")
+    .doc(uid)
+    .update({ imageUrl: downloadUrl });
 }
--- functions/src/update-functions.ts
+++ functions/src/update-functions.ts
@@ -1,43 +1,63 @@
-import * as admin from 'firebase-admin';
-import {DocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
-import {Change} from 'firebase-functions/lib/cloud-functions';
-import {EventContext} from 'firebase-functions';
-import * as _ from 'lodash';
-import {Invoice, InvoiceFactory} from 'jovisco-domain';
-import {getRevenues, setRevenues} from '../../shared/src';
+import * as admin from "firebase-admin";
+// import {DocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
+// import {Change} from 'firebase-functions/lib/cloud-functions';
+import { Change, EventContext } from "firebase-functions";
+import * as _ from "lodash";
+import { Invoice, InvoiceFactory } from "jovisco-domain";
+import { getRevenues, setRevenues } from "../../shared/src";
+import { DocumentSnapshot } from "firebase-functions/v1/firestore";
 
-export async function handleReceiverCreation(snap: DocumentSnapshot, context: EventContext): Promise<any> {
-  return updateNumberRange('receivers', context);
+export async function handleReceiverCreation(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
+  return updateNumberRange("receivers", context);
 }
 
-export async function handleReceiverDeletion(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleReceiverDeletion(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   // reset number range
-  await resetNumberRange('receivers', context);
+  await resetNumberRange("receivers", context);
   // delete assigned document links
-  return deleteDocumentLinksForOwner(`${snap.data().objectType}/${context.params.id}`);
+  return deleteDocumentLinksForOwner(
+    `${snap.data().objectType}/${context.params.id}`
+  );
 }
 
-export async function handleContractCreation(snap: DocumentSnapshot, context: EventContext): Promise<any> {
-    return updateNumberRange('contracts', context);
+export async function handleContractCreation(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
+  return updateNumberRange("contracts", context);
 }
 
-export async function handleContractDeletion(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleContractDeletion(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   try {
     // reset number range
-    await resetNumberRange('contracts', context);
+    await resetNumberRange("contracts", context);
     // delete assigned document links
-    return deleteDocumentLinksForOwner(`${snap.data().objectType}/${context.params.id}`);
+    return deleteDocumentLinksForOwner(
+      `${snap.data().objectType}/${context.params.id}`
+    );
   } catch (err) {
     console.error(err);
   }
 }
 
-export async function handleInvoiceCreation(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleInvoiceCreation(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   try {
     // update number range
-    let rangeId = 'invoices';
+    let rangeId = "invoices";
     if (snap.data().billingMethod === 1) {
-      rangeId = 'credit-requests';
+      rangeId = "credit-requests";
     }
     await updateNumberRange(rangeId, context);
     // update revenue reporting
@@ -47,16 +67,21 @@ export async function handleInvoiceCreation(snap: DocumentSnapshot, context: Eve
   }
 }
 
-export async function handleInvoiceDeletion(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleInvoiceDeletion(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   try {
     // reset number range
-    let rangeId = 'invoices';
+    let rangeId = "invoices";
     if (snap.data().billingMethod === 1) {
-      rangeId = 'credit-requests';
+      rangeId = "credit-requests";
     }
     await resetNumberRange(rangeId, context);
     // delete assigned document links
-    await deleteDocumentLinksForOwner(`${snap.data().objectType}/${context.params.id}`);
+    await deleteDocumentLinksForOwner(
+      `${snap.data().objectType}/${context.params.id}`
+    );
     // update revenue reporting
     return updateRevenueReporting(null, snap.data());
   } catch (err) {
@@ -64,7 +89,10 @@ export async function handleInvoiceDeletion(snap: DocumentSnapshot, context: Eve
   }
 }
 
-export async function handleInvoiceUpdate(change: Change<DocumentSnapshot>, context: EventContext): Promise<any> {
+export async function handleInvoiceUpdate(
+  change: Change<DocumentSnapshot>,
+  context: EventContext
+): Promise<any> {
   try {
     // get old and new state
     const newInvoice = change.after.data();
@@ -79,20 +107,31 @@ export async function handleInvoiceUpdate(change: Change<DocumentSnapshot>, cont
   }
 }
 
-export async function handleUserProfileCreation(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleUserProfileCreation(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   const uid = context.params.id;
   const userProfile = snap.data();
   return updateAuth(uid, userProfile);
 }
-export async function handleUserProfileDeletion(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleUserProfileDeletion(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   try {
     const uid = context.params.id;
-    return admin.auth().setCustomUserClaims(uid, {isAdmin: false, isSales: false});
+    return admin
+      .auth()
+      .setCustomUserClaims(uid, { isAdmin: false, isSales: false });
   } catch (err) {
     console.log(err);
   }
 }
-export async function handleUserProfileUpdate(change: Change<DocumentSnapshot>, context: EventContext): Promise<any> {
+export async function handleUserProfileUpdate(
+  change: Change<DocumentSnapshot>,
+  context: EventContext
+): Promise<any> {
   try {
     const uid = context.params.id;
     const userProfile = change.after.data();
@@ -102,7 +141,10 @@ export async function handleUserProfileUpdate(change: Change<DocumentSnapshot>, 
   }
 }
 
-export async function handleDocumentLinkDeletion(snap: DocumentSnapshot, context: EventContext): Promise<any> {
+export async function handleDocumentLinkDeletion(
+  snap: DocumentSnapshot,
+  context: EventContext
+): Promise<any> {
   try {
     const documentLink = snap.data();
     // delete document from storage
@@ -115,16 +157,20 @@ export async function handleDocumentLinkDeletion(snap: DocumentSnapshot, context
 }
 
 async function deleteDocumentLinksForOwner(owner: string): Promise<any> {
-  const docs = await admin.firestore().collection('document-links')
-    .where('owner', '==', owner)
+  const docs = await admin
+    .firestore()
+    .collection("document-links")
+    .where("owner", "==", owner)
     .get();
   const batch = admin.firestore().batch();
-  docs.forEach(doc => batch.delete(doc.ref));
+  docs.forEach((doc) => batch.delete(doc.ref));
   return batch.commit();
 }
 
-function isInvoiceUpdateRelevantForRevenue(newInvoiceData, oldInvoiceData): boolean {
-
+function isInvoiceUpdateRelevantForRevenue(
+  newInvoiceData,
+  oldInvoiceData
+): boolean {
   let newInvoice: Invoice, oldInvoice: Invoice;
 
   if (_.isEqual(newInvoiceData, oldInvoiceData)) {
@@ -155,21 +201,27 @@ function isInvoiceUpdateRelevantForRevenue(newInvoiceData, oldInvoiceData): bool
   return false;
 }
 
-async function resetNumberRange(rangeId: string, context: EventContext): Promise<any> {
+async function resetNumberRange(
+  rangeId: string,
+  context: EventContext
+): Promise<any> {
   const id = context.params.id;
-  const rangeRef = admin.firestore().collection('number-ranges').doc(rangeId);
+  const rangeRef = admin.firestore().collection("number-ranges").doc(rangeId);
   const range = await rangeRef.get();
   if (range.data().lastUsedId === id) {
     const lastId = (+id - 1).toString();
-    return rangeRef.update({lastUsedId: lastId});
+    return rangeRef.update({ lastUsedId: lastId });
   }
   return false;
 }
 
-async function updateNumberRange(rangeId: string, context: EventContext): Promise<any> {
+async function updateNumberRange(
+  rangeId: string,
+  context: EventContext
+): Promise<any> {
   const id = context.params.id;
-  const rangeRef = admin.firestore().collection('number-ranges').doc(rangeId);
-  return rangeRef.update({lastUsedId: id});
+  const rangeRef = admin.firestore().collection("number-ranges").doc(rangeId);
+  return rangeRef.update({ lastUsedId: id });
 }
 
 async function updateAuth(uid: string, userProfile: any): Promise<any> {
@@ -178,20 +230,40 @@ async function updateAuth(uid: string, userProfile: any): Promise<any> {
       displayName: userProfile.displayName,
       disabled: userProfile.isLocked,
       phoneNumber: userProfile.phoneNumber,
-      photoURL: userProfile.imageUrl && userProfile.imageUrl.length ? userProfile.imageUrl : null
+      photoURL:
+        userProfile.imageUrl && userProfile.imageUrl.length
+          ? userProfile.imageUrl
+          : null,
     });
-    const isAdmin = userProfile.roles.indexOf('sys-admin') >= 0;
-    const isSales = userProfile.roles.indexOf('sales-user') >= 0;
-    return admin.auth().setCustomUserClaims(uid, {isAdmin: isAdmin, isSales: isSales});
+    const isAdmin = userProfile.roles.indexOf("sys-admin") >= 0;
+    const isSales = userProfile.roles.indexOf("sales-user") >= 0;
+    return admin
+      .auth()
+      .setCustomUserClaims(uid, { isAdmin: isAdmin, isSales: isSales });
   } catch (err) {
     throw new Error(err);
   }
 }
-async function updateRevenueReporting(newInvoiceData: any, oldInvoiceData: any): Promise<any> {
+async function updateRevenueReporting(
+  newInvoiceData: any,
+  oldInvoiceData: any
+): Promise<any> {
   const db = admin.firestore();
   const values = {
-    new: { year: null, month: null, receiverId: null, organization: null, revenue: 0 },
-    old: { year: null, month: null, receiverId: null, organization: null, revenue: 0 },
+    new: {
+      year: null,
+      month: null,
+      receiverId: null,
+      organization: null,
+      revenue: 0,
+    },
+    old: {
+      year: null,
+      month: null,
+      receiverId: null,
+      organization: null,
+      revenue: 0,
+    },
   };
   if (newInvoiceData) {
     // get new values
@@ -223,36 +295,55 @@ async function updateRevenueReporting(newInvoiceData: any, oldInvoiceData: any):
       revenues = {
         id: values.new.year,
         organization: values.new.organization,
-        months: {[values.new.month]: {[values.new.receiverId]: values.new.revenue}}
+        months: {
+          [values.new.month]: { [values.new.receiverId]: values.new.revenue },
+        },
       };
     } else if (!revenues.months[values.new.month]) {
       // create new entry for month
-      revenues.months[values.new.month] = {[values.new.receiverId]: values.new.revenue};
+      revenues.months[values.new.month] = {
+        [values.new.receiverId]: values.new.revenue,
+      };
     } else if (!revenues.months[values.new.month][values.new.receiverId]) {
       // create new entry for receiver
-      revenues.months[values.new.month][values.new.receiverId] = values.new.revenue;
+      revenues.months[values.new.month][values.new.receiverId] =
+        values.new.revenue;
     } else {
       // add to existing entry
-      revenues.months[values.new.month][values.new.receiverId] += values.new.revenue;
+      revenues.months[values.new.month][values.new.receiverId] +=
+        values.new.revenue;
     }
-    console.log('correct revenue entry: ', values.new.month, values.new.receiverId, revenues.months[values.new.month][values.new.receiverId]);
+    console.log(
+      "correct revenue entry: ",
+      values.new.month,
+      values.new.receiverId,
+      revenues.months[values.new.month][values.new.receiverId]
+    );
     await setRevenues(db, revenues);
   }
 
   if (values.old.year) {
     const docId = `${values.old.year}_${values.old.organization}`;
     const revenues = await getRevenues(db, docId);
-    if (revenues && revenues.months[values.old.month] && revenues.months[values.old.month][values.old.receiverId]) {
+    if (
+      revenues &&
+      revenues.months[values.old.month] &&
+      revenues.months[values.old.month][values.old.receiverId]
+    ) {
       // correct old entry
       revenues.months[values.old.month][values.old.receiverId] = Math.max(
-        revenues.months[values.old.month][values.old.receiverId] - values.old.revenue,
+        revenues.months[values.old.month][values.old.receiverId] -
+          values.old.revenue,
         0
       );
-      console.log('correct revenue entry: ', values.old.month, values.old.receiverId, revenues.months[values.old.month][values.old.receiverId]);
+      console.log(
+        "correct revenue entry: ",
+        values.old.month,
+        values.old.receiverId,
+        revenues.months[values.old.month][values.old.receiverId]
+      );
       await setRevenues(db, revenues);
     }
   }
   return true;
 }
-
-
